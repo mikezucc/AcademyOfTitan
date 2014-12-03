1f58b6810dd236154920b423254a66794f4e8f33
mikezucc <mikezuccarino@gmail.com>
2014-12-02 21:01:26 -0800

    somethings working?

---
+++ Project/Project/DefaultWindow.h
@@ -0,0 +1,13 @@
+//
+//  DefaultWindow.h
+//  Project
+//
+//  Created by ASCI-Red on 11/10/14.
+//  Copyright (c) 2014 Earth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DefaultWindow : UIViewController
+
+@end
---
+++ Project/Project/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  Project
+//
+//  Created by ASCI-Red on 11/10/14.
+//  Copyright (c) 2014 Earth. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    IBOutlet UIButton *startButton;
+    
+    
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *btnImage = [UIImage imageNamed:@"burg_eltz_deviantart_by_gingado-d83yi7h.jpg"];
+    [startButton setImage:btnImage forState:UIControlStateNormal];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
